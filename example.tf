cdac18ffdb53f5bfe00257db9cf253c66d115f12
tugi1962 <67828512+tugi1962@users.noreply.github.com>
2023-02-04 17:21:43 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARHZVEAJOOVLHLBOC"
-  secret_key = "Sm7DZaQSIkFsWicuQnWIbAB3eLOVlN5Ct3LlPtDY"
+  access_key = "...."
+  secret_key = "..."
 }
 
 resource "aws_instance" "ec2" {
